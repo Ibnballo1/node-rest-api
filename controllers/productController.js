86b71b78483a7161ac64218d420473697378af7d
Ibnballo1 <belloabdullateef035@gmail.com>
2025-10-27 17:42:18 +0100

    Add get request

---
+++ controllers/productController.js
@@ -0,0 +1,38 @@
+const Product = require("../modals/productModal");
+
+/// @Desc GET ALL PRODUCTS
+/// @Route GET /api/products
+/// @Access Public
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/// @Desc GET SINGLE PRODUCT
+/// @Route GET /api/products/:id
+/// @Access Public
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+      return;
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct,
+};
